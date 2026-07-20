ca8867f8191e4d5e2edc9e2d133f54e8b5a11893
Jean <info@jlouis.me>
2026-07-21 00:14:41 +0200

    update app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { authGuard } from './auth-guard';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'users' },
+  {
+    path: 'login',
+    loadComponent: () => import('./login').then((m) => m.Login),
+  },
+  {
+    path: 'users',
+    canActivate: [authGuard],
+    loadComponent: () => import('./manage-users').then((m) => m.ManageUsers),
+  },
+  { path: '**', redirectTo: 'users' },
+];
